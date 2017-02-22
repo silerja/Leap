b093cae551694b93056da6844ea8a2a9082cb631
Joseph Siler <Joseph Siler>
2017-02-22 11:00:15 -0500

    updated code

--- spec/isLeapYear.js
+++ spec/isLeapYear.js
@@ -1,8 +1,11 @@
 // define a leap year function
 var isLeapYear = (year) => {
-	// is year divisible by 4
-	return (0 === year % 4);
-};
+	
+    if(0 === year % 400 || (0 === % 4 && 0 !== year % 100 )){
+    	return true;	
+    }
+    return false;
+};    
 
 // export isLeapYear for use in other contracts
 module.exports = isLeapYear;
--- spec/leap_spec.js
+++ spec/leap_spec.js
@@ -1,7 +1,7 @@
 // import the code to the test
 var isLeapYear = require("../index");
 
-describe("A program to determine if a year is a leap year"),()=>{
+describe("A program to determine if a year is a leap year",()=>{
 
     it("define a function called isLeapYear", () => {
 
@@ -14,19 +14,25 @@ describe("A program to determine if a year is a leap year"),()=>{
 
         // the years were chosen by random with a couple from
         // each centrury; referene Gregorian calander
-	    expect(isLeapYear(1817).tobe(false);
-	    expect(isLeapYear(1816).tobe(true);
-	    expect(isLeapYear(1927).tobe(false);
-	    expect(isLeapYear(1928).tobe(true);
-	    expect(isLeapYear(2034).tobe(false);
-	    expect(isLeapYear(2040).tobe(true);
-	    expect(isLeapYear(2154).tobe(false);
-	    expect(isLeapYear(2160).tobe(true);
+	    expect(isLeapYear(1817).tobe(false));
+	    expect(isLeapYear(1816).tobe(true));
+	    expect(isLeapYear(1927).tobe(false));
+	    expect(isLeapYear(1928).tobe(true));
+	    expect(isLeapYear(2034).tobe(false));
+	    expect(isLeapYear(2040).tobe(true));
+	    expect(isLeapYear(2154).tobe(false));
+	    expect(isLeapYear(2160).tobe(true));
     });
 
     it("returns false for years divisible by 100", () => {
         //years chosen by random fitting example
-        expect(isLeapYear(1800).tobe(false);
+        expect(isLeapYear(1800).tobe(false));
+        expect(isLeapYear(1900).toBe(false));
+        expect(isLeapYear(1801).tobe(true));  
+    });
 
+    it("Returns true for years divisible by 400", () => {
+        expect(isLeapYear(800).tobe(true));
+        expect(isLeapYear(801).tobe(false));
     });
 });
